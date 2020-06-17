a3e350ff28143824eebf01d650829f88a72deae4
Sarthak Khandelwal <sarthakk5678@gmail.com>
2020-06-17 16:09:33 +0530

    commit

---
+++ 03-add two numbers.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+void main()
+{
+    int a,b;
+
+    printf("Enter the value of two numbers\n");
+    scanf("%d %d", &a, &b);
+
+    printf("The sum of numbers is %d",a + b);
+}
