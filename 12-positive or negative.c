03bc32473d791f9dc5140b0d1985d118b0c6ddb2
Sarthak Khandelwal <sarthakk5678@gmail.com>
2020-06-21 14:31:25 +0530

    upload file

---
+++ 12-positive or negative.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main()
+{   
+    int a;
+    
+    printf("Enter a number\n");
+    scanf("%d", &a);
+
+    if(a > 0)
+    	printf("The number a is positive.");
+    
+    else if(a < 0)
+    	printf("The number a is negative.");
+    
+    else
+    	printf("The number a is zero.");
+}
+   
