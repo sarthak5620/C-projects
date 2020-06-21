a4ccb838e08b7bf2d3dc22fa6721d66897ed601d
Sarthak Khandelwal <sarthakk5678@gmail.com>
2020-06-21 14:39:53 +0530

    upload file

---
+++ 13-shorthand operator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+{    
+    int a = 10, b = 10;
+    
+    printf("Initially value of a is %d and b is %d\n", a, b);
+    
+    b += a;
+    printf("Value of b after b += a is %d\n", b);
+    
+    b -= a;
+    printf("value of b after b -= a is %d\n", b);
+    
+    b *= a;
+    printf("Value of b after b *= a is %d\n", b);
+    
+    b /= a;
+    printf("Value of b after b /= a is %d\n", b);
+    
+    b %= a;
+    printf("Value of b after b %%= a is %d\n", b);
+}
