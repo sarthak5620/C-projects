8a79158b1d3139dd4e9fd00d09274a9addd20276
Sarthak Khandelwal <sarthakk5678@gmail.com>
2020-06-22 23:04:38 +0530

    upload file

---
+++ 16-fahrenheit to celsius converter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    float f, c;
+    
+    printf("Enter the temperature in Fahrenheit\n");
+    scanf("%f", &f);
+    
+    c = (f-32)*5/9;
+    printf("The temperature is %f degree calcius", c);
+}
