15bf030875a39f26cb102092f2c1088f03bb160d
Sarthak Khandelwal <sarthakk5678@gmail.com>
2020-06-29 14:31:08 +0530

    upload file

---
+++ 22-armstrong number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+    int number, temp, remainder, sum = 0;
+    printf("Enter a number\n");
+    scanf("%d", &number);
+
+    temp = number;
+
+    while(temp > 0){
+        remainder = temp%10;
+        sum += remainder*remainder*remainder;
+        temp /= 10;
+    }
+    (sum == number) ? printf("The number is Armstrong number") : printf("The number is not Armstrong number");
+}
